2449f69eedb16813a43eae23641bbcd30a68a6ca
gurung69 <gurungkrish44@gmail.com>
2024-05-31 14:04:21 -0600

    added function to get client coordinate

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [coords, setCoords] = useState(null);
+
+  function getLocation(){
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            setCoords([position.coords.latitude, position.coords.longitude]);
+        })
+    }
+  }
+
+  useEffect(()=>{
+      getLocation();
+  }, [])
+
   return (
     <div className="App">
     </div>
