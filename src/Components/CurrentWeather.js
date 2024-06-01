8fa34cbf8d1dca953a47ea8707afd405db6c036e
gurung69 <gurungkrish44@gmail.com>
2024-06-01 13:20:17 -0600

    add style to the site

--- src/Components/CurrentWeather.js
+++ src/Components/CurrentWeather.js
@@ -5,15 +5,16 @@ export default function CurrentWeather({data}){
     return(
         <div className="current-info-container">
             <div className="current-info-left">
-                <h1 className="location">{data.name}</h1>
+                <h1 className="location">{data.name}, {data.sys.country}</h1>
                 <img src={`${iconUrl}\\${data.weather[0].icon}@2x.png`}></img>
-                <h1 className="current-weather-description">{data.weather[0].description}</h1>
+                <h1 className="current-weather-condition">{data.weather[0].description}</h1>
             </div>
             <div className="current-info-right">
-                <h1 className="temp">{Math.floor(data.main.temp)} °C</h1>
+                <p className="temp"><span className="bold">{Math.floor(data.main.temp)}</span> °C</p>
                 <p>Feels Like: {Math.floor(data.main.feels_like)} °C</p>
                 <p>Humidity: {data.main.humidity}%</p>
                 <p>Wind Speed: {data.wind.speed} km/h</p>
+                <p>Pressure: {data.main.pressure} kPa</p>
             </div>
         </div>
     )
--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -5,10 +5,16 @@ export default function SearchBar({city, updateCity}) {
     function handleChange(e){
         updateCity(e.target.value);
     }
+
+    function handleSubmission(e){
+        e.preventDefault();
+    }
     return(
         <div className="search-bar">
-            <FaSearch /> 
-            <input type="text" className="search" placeholder="Find a City..." value={city} onChange={handleChange}></input>
+            <form onSubmit={handleSubmission}>
+                <FaSearch style={{position: "absolute", fontSize: "17px", margin: "8px 0 0 10px"}}/> 
+                <input type="text" className="search" placeholder="Find a City..." value={city} onChange={handleChange}></input>
+            </form>
         </div>
     )
 }
